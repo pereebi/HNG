f1db17cc74c738cc587d1e47fb53c3be8be98bc8
Pereebi Egbekun <pereebiegebkun@gmail.com>
2022-10-29 00:11:02 +0100

    commiting changes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
   
 // the get enpoint that returns the json response
-app.get('/', (request, response)=>{
+app.get('/perry-server', (request, response)=>{
    response.status(200).json({
     "slackUsername": "Pere-ebi", "backend": true, "age": 28, "bio": "I love to eat, read, travel and be creative with my hands"
    })
